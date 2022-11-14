fddae7f7087cef3d37150cc9bad63c1d8ff0a087
Lenovo <Lenovo@DESKTOP-620RG2R>
2022-11-13 21:45:36 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio2.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace litrosdeleche
+{
+
+    public static void Main()
+    {
+
+        double LV, PG, CG, VV;
+        double LPG = 3.785; 
+
+        Console.WriteLine("Dijite los litros de la venta");
+        LV = double.Parse(Console.ReadLine());
+        Console.WriteLine("Dijite el precio del galon");
+        PG = double.Parse(Console.ReadLine());
+        CG = LV / LPG;
+        VV = CG * PG;
+        Console.WriteLine($"El valor de venta es {VV}");
+        Console.WriteLine($"La cantidad de galones es {CG}");
+
+    }
+        
+}
